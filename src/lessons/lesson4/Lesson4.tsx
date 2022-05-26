3d0a02efb8f97db546bf34a14cbaed4c61cb0637
egory4fash <egormaksul@gmail.com>
2022-05-26 11:52:32 +0300

    12 done

--- src/lessons/lesson4/Lesson4.tsx
+++ src/lessons/lesson4/Lesson4.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './lesson_4';
-import {createHandler, rejectHandler, resolveHandler} from "./lesson_4";
+// import {createHandler, rejectHandler, resolveHandler} from "./lesson_4";
 
 const Lesson4 = () => {
     return (
         <div>
-            <button id='btn-create-promise' onClick={createHandler}>Create Promise</button>
-            <button id='btn-resolve-promise' onClick={resolveHandler}>Resolve Promise</button>
-            <button id='btn-reject-promise' onClick={rejectHandler}>Reject Promise</button>
+            {/*<button id='btn-create-promise' onClick={createHandler}>Create Promise</button>*/}
+            {/*<button id='btn-resolve-promise' onClick={resolveHandler}>Resolve Promise</button>*/}
+            {/*<button id='btn-reject-promise' onClick={rejectHandler}>Reject Promise</button>*/}
         </div>
     );
 }
--- src/lessons/lesson4/lesson_4.ts
+++ src/lessons/lesson4/lesson_4.ts
@@ -1,3 +1,5 @@
+import {log} from "util";
+
 console.log('lesson 4');
 
 // http://latentflip.com/loupe/?code=JC5vbignYnV0dG9uJywgJ2NsaWNrJywgZnVuY3Rpb24gb25DbGljaygpIHsKICAgIHNldFRpbWVvdXQoZnVuY3Rpb24gdGltZXIoKSB7CiAgICAgICAgY29uc29sZS5sb2coJ1lvdSBjbGlja2VkIHRoZSBidXR0b24hJyk7ICAgIAogICAgfSwgMjAwMCk7Cn0pOwoKY29uc29sZS5sb2coIkhpISIpOwoKc2V0VGltZW91dChmdW5jdGlvbiB0aW1lb3V0KCkgewogICAgY29uc29sZS5sb2coIkNsaWNrIHRoZSBidXR0b24hIik7Cn0sIDUwMDApOwoKY29uc29sZS5sb2coIldlbGNvbWUgdG8gbG91cGUuIik7!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
@@ -63,23 +65,44 @@ console.log('lesson 4');
 // свойства resolve и reject получают ссылки на соответствующие функции
 // resolve и reject. Следующие два обработчика запускают методы resolve и reject.
 
-let handlePromise = {
-    promise:null,
-    resolve:null,
-    reject:null,
-    onSuccess (param:any) {
-        console.log(`Promise is resolved with data: ${param}`)
-    },
-    onError (param:any) {`Promise is rejected with error: ${param}`}
-}
-
-export const createHandler = () => {
-    let buttonPromise = new Promise((resolve, reject) => {
-        this.promise = buttonPromise
-    })
-}
-export const resolveHandler = () => {}
-export const rejectHandler = () => {}
+// type testObj = {
+//     promise: null | Promise<any>,
+//     resolve: null | Function,
+//     reject: null | Function,
+//     onSuccess: (param: string) => void
+//     onError: (param: string) => void
+// }
+//
+// let handlePromise: testObj = {
+//     promise: null,
+//     resolve: null,
+//     reject: null,
+//     onSuccess(param) {
+//         console.log(`Promise is resolved with data: ${param}`)
+//     },
+//     onError(param) {
+//         console.log(`Promise is rejected with error: ${param}`)
+//     }
+// }
+//
+// export const createHandler = () => {
+//     handlePromise.promise = new Promise((res, rej) => {
+//         handlePromise.resolve = res;
+//         handlePromise.reject = rej;
+//     })
+//     handlePromise.promise
+//         .then(handlePromise.onSuccess)
+//         .catch(handlePromise.onError)
+// }
+// export const resolveHandler = () => {
+//     handlePromise.resolve && handlePromise.resolve("yo")
+// }
+// export const rejectHandler = () => {
+//     handlePromise.reject && handlePromise.reject('nope')
+// }
+//
+// //@ts-ignore
+// window.testObj = handlePromise
 
 
 // Task 06
@@ -89,12 +112,51 @@ export const rejectHandler = () => {}
 // Создайте функцию print, которая выводит в консоль значение своего параметра
 // Добавьте два метода then и передайте созданные функции.
 
-
+// const promise = new Promise((res,rej) => {
+//     setTimeout( () => {
+//         res('My Name Is')
+//     },5000)
+// })
+//
+// const onSuccess = (param:any) => {
+//     return `${param}  Egor`
+// }
+// const Print = (param:any) => {
+//     console.log(param)
+// }
+//
+// promise.then(onSuccess).then(Print)
 // Task 7
 // Создайте три промиса. Первый промис возвращает объект { name: "Anna" } через 2с,
 // второй промис возвращает объект {age: 16} через 3 с, а третий {city: ''} через 4с.
 // Получите результаты работы промисов, объедините свойства объектов
 // и выведите в консоль {name, age, city}
+
+const name = new Promise((res) => {
+    setTimeout(() => {
+        res({name: 'Anna'})
+    }, 2000)
+})
+const age = new Promise((res) => {
+    setTimeout(() => {
+        res({age: 16})
+    }, 3000)
+})
+const city = new Promise((res) => {
+    setTimeout(() => {
+        res({city: 'Minsk'})
+    }, 4000)
+})
+
+let person = {}
+Promise.all([name, age, city])
+    .then((res) => {
+        res.map(m => {
+            Object.assign(person, m)
+        })
+    })
+
+console.log(person)
 
 
 // just a plug
